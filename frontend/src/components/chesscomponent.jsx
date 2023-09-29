d50674464dd580bcc543842aae4e61628b7836e3
Praneeth1 G <friendpraneeth@gmail.com>
2023-09-29 08:54:09 +0530

    fixed bugs

--- frontend/src/components/chesscomponent.jsx
+++ frontend/src/components/chesscomponent.jsx
@@ -412,7 +412,7 @@ const ChessboardComponent = () => {
       <button disabled={gameOver} onClick={handleRequestDraw}>Request for a Draw</button>
       <button disabled={gameOver} onClick={handleResign}>Resign</button>
       {/* <p>{allowMyself ? "Your Turn" : (gameOver? "" : "Waiting for opponent")}</p> */}
-      {allowMyself ? <b style={{color:"green"}}>Your Turn</b> : (gameOver ? <p></p> : <b>Waiting for opponent</b>)}
+      <p>{allowMyself ? <b style={{color:"green"}}>Your Turn</b> : (gameOver ? <p></p> : <b>Waiting for opponent</b>)}</p>
       <p>{gameOver? "Game Over" : ""}</p>
       <p>{winner}  {result}</p>
       <p>{pgn}</p>
